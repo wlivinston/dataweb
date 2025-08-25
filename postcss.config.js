6ef34f9e131737dceb829fd1000e2f86e5861249
DataAfrik Admin <admin@dataafrik.com>
2025-08-25 17:47:13 -0400

    Fix: Move Tailwind CSS to dependencies and simplify PostCSS config for DigitalOcean

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import postcssImport from 'postcss-import';
-
 export default {
-  plugins: [
-    postcssImport,
-    tailwindcss,
-    autoprefixer,
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
